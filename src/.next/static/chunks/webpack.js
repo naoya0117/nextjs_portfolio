0bd8108a3465b54ad80baa512790d708919fc652
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2023-10-11 18:01:43 +0900

    ホームページの更新

--- src/.next/static/chunks/webpack.js
+++ src/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1139edf09b360387"; }
+/******/ 		__webpack_require__.h = function() { return "bb6a4042129e2b64"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>naoya0117</title>
+      </Head>
       <h1>こんにちは</h1>
+      <p>これはブログです</p>
     </>
   )
 }
