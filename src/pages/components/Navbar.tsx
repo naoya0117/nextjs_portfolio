2115262f5a83c83720858c42a042f5c8e86bf149
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2023-10-19 01:09:25 +0900

    全体的なレイアウトの変更

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from '../../styles/Navbar.module.css';
 
 
 export default function Navbar() {
   return (
-    <nav className={styles.nav}>
-      <ul>
-        <li><Link href="/">TOP</Link></li>
-        <li><Link href="/portfolio">SKILL</Link></li>
-        <li><Link href="/contact">BLOG</Link></li>
-      </ul>
-    </nav>
+    <div className={styles.nav}>
+        <div className={styles.navList}>
+            <div className={styles.logo}>
+                <Link href={"/"}>
+                    <Image src={"/icon/logo.png"}
+                           width={50}  height={50}
+                           alt={"logo"}/>
+                    <p>　Blog</p>
+                </Link>
+            </div>
+            <ul>
+                <li><Link href="/">TOP</Link></li>
+                <li><Link href="/portfolio">PORTFOLIO</Link></li>
+                <li><Link href="/contact">BLOG</Link></li>
+            </ul>
+        </div>
+    </div>
   );
 }
